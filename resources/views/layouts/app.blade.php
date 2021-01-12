c38ef5b214e7d1cb7fb1dfbb3f47108761e3845d
Wagner Gomes Gonçalves <43182168+wagnergoncalves@users.noreply.github.com>
2021-01-11 21:37:29 -0300

    Ajustes do cliente

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,106 +2,13 @@
 <html lang="{{ app()->getLocale() }}" translate="no">
 
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name', 'SIPAT 2020') }}</title>
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
-    <meta name="google" content="notranslate">
-    <script src="https://kit.fontawesome.com/2c7146b1c7.js" crossorigin="anonymous"></script>
-
+    @include('layouts.head')
 </head>
 
 <body class="bg-banner">
     <div id="app" style="background-color: white">
 
-
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            @guest
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    LMS Aldeia Consultoria
-                </a>
-            @else
-                <a class="navbar-brand" href="{{ url('/home') }}">
-                    LMS Aldeia Consultoria
-                </a>
-            @endguest
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
-                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div style="width: 100%">
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li><a class="nav-link " href="{{ route('login') }}">{{ __('Faça o login') }}</a></li>
-                            <!-- <li><a class="nav-link" href="{{ route('register') }}">{{ __('Cadastre-se') }}</a></li> -->
-                        @else
-                            
-                            <li><span class="nav-link font-weight-bold">{{ Auth::user()->name }}</span></li>
-                            <li><a class="nav-link" href="{{ route('home') }}">Acessar Curso</a></li>
-                            @role('Admin|Gestor')
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    Administração
-                                </a>
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('users.index') }}">Usuários</a>
-                                    @role('Gestor')
-                                    <a class="dropdown-item" href="{{ route('matriculas.index') }}">Matrículas</a>
-                                    @endrole
-                                    @role('Admin')
-                                    <div class="dropdown-divider"></div>
-                                    <a class="dropdown-item small"><strong>Clientes</strong></a>
-                                    <a class="dropdown-item" href="{{ route('planos.index') }}">Planos</a> 
-                                    <a class="dropdown-item" href="{{ route('empresas.index') }}">Empresas</a>                                    
-                                    <a class="dropdown-item" href="{{ route('matriculas.index') }}">Matrículas</a>
-                                    <div class="dropdown-divider"></div>
-                                    <a class="dropdown-item small"><strong>Conteúdo</strong></a>
-                                    <a class="dropdown-item" href="{{ route('modulos.index') }}">Módulos</a>
-                                    <a class="dropdown-item" href="{{ route('aulas.index') }}">Aulas</a>
-                                    <a class="dropdown-item" href="{{ route('materiais.index') }}">Materiais</a>
-                                    <div class="dropdown-divider"></div>
-                                    <a class="dropdown-item small"><strong>Feedbacks</strong></a>
-                                    <a class="dropdown-item" href="{{ route('questionarios.index') }}">Questionários</a>
-                                    <a class="dropdown-item" href="{{ route('perguntas.index') }}">Perguntas</a>
-                                    <a class="dropdown-item" href="{{ route('respostas.index') }}">Respostas</a>
-                                    @endrole
-                                </div>
-                            </li>
-                            @endrole
-
-                            <li><a class="nav-link" href="{{ route('userslogged.index') }}">Meus dados</a></li>
-                            <li><a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                        document.getElementById('logout-form').submit();">Sair</a>
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    @csrf
-                                </form>
-                            </li>
-
-
-                        @endguest
-                    </ul>
-                </div>
-                @if (Request::path() == 'quizzes/responder' && isset($quizz))
-                    <h4 class="pt-4 text-right">{!! $quizz->nome !!}</h4>
-                @endif
-            </div>
-        </nav>
-
-
-
+        @include('layouts.menu')
 
         <main class="py-4">
             <div class="container">
@@ -109,19 +16,20 @@
             </div>
         </main>
 
-        <!-- FOOTER -->
-        <footer>
-            <div class="container-fluid bg-banner">
-                <div class="row">
-                    <div class="col-md-12 py-3 px-3 text-white font-weight-bold text-center">
-                        Aldeia Consultoria © {{ date('Y') }} | Todos os direitos reservados.
-                    </div>
-                </div>
-            </div>
-            <footer>
+        @include('layouts.footer')
 
     </div>
-
+    <script>
+        $(window).load(function(){
+            $.ajaxSetup({
+                statusCode: {
+                    419: function(){
+                            location.reload(); 
+                        }
+                }
+            });
+        });
+    </script>
 </body>
 
 </html>
